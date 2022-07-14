3be8b10896b4ed1e7774554b36dcacfe1cbb91af
Yao Jean-Elisée <37454954+JeanElisee@users.noreply.github.com>
2022-07-14 14:56:07 +0200

    Adding project files

---
+++ src/main/java/com/addictmobile/romannumber/RomannumberApplication.java
@@ -0,0 +1,13 @@
+package com.addictmobile.romannumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomannumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RomannumberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/addictmobile/romannumber/RomannumberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.addictmobile.romannumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomannumberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
